ce8636b2a4565c9e68fb6d0505a34b5654d1cc75
prashantgh111 <107759833+prashantgh111@users.noreply.github.com>
2023-04-28 22:28:57 +0530

    food dilevery added

--- src/main/java/com/driver/ui/controller/FoodController.java
+++ src/main/java/com/driver/ui/controller/FoodController.java
@@ -1,10 +1,17 @@
 package com.driver.ui.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import com.driver.io.entity.FoodEntity;
+import com.driver.io.repository.FoodRepository;
 import com.driver.model.request.FoodDetailsRequestModel;
 import com.driver.model.response.FoodDetailsResponse;
 import com.driver.model.response.OperationStatusModel;
+import com.driver.service.FoodService;
+import com.driver.shared.dto.FoodDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,33 +25,107 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/foods")
 public class FoodController {
 
+	@Autowired
+	FoodService foodService;
+
+	@Autowired
+	FoodRepository foodRepository;
+
 	@GetMapping(path="/{id}")
 	public FoodDetailsResponse getFood(@PathVariable String id) throws Exception{
 
-		return null;
+		FoodEntity foodEntity = foodRepository.findById(Long.parseLong(id)).get();
+		String foodId = foodEntity.getFoodId();
+
+		FoodDto foodDto = foodService.getFoodById(foodId);
+
+		FoodDetailsResponse foodDetailsResponse = new FoodDetailsResponse();
+		foodDetailsResponse.setFoodPrice(foodDto.getFoodPrice());
+		foodDetailsResponse.setFoodName(foodDto.getFoodName());
+		foodDetailsResponse.setFoodCategory(foodDto.getFoodCategory());
+		foodDetailsResponse.setFoodId(foodDto.getFoodId());
+
+		return foodDetailsResponse;
 	}
 
 	@PostMapping("/create")
 	public FoodDetailsResponse createFood(@RequestBody FoodDetailsRequestModel foodDetails) {
 
-		return null;
+		FoodDto foodDto  = new FoodDto();
+		foodDto.setFoodCategory(foodDetails.getFoodCategory());
+		foodDto.setFoodId(UUID.randomUUID().toString());
+		foodDto.setFoodName(foodDetails.getFoodName());
+		foodDto.setFoodPrice(foodDetails.getFoodPrice());
+
+		FoodDto foodDto1 = foodService.createFood(foodDto);
+		FoodDetailsResponse foodDetailsResponse = new FoodDetailsResponse();
+		foodDetailsResponse.setFoodCategory(foodDto1.getFoodCategory());
+		foodDetailsResponse.setFoodName(foodDto1.getFoodName());
+		foodDetailsResponse.setFoodPrice(foodDto1.getFoodPrice());
+		foodDetailsResponse.setFoodId(foodDto1.getFoodId());
+
+
+
+		return foodDetailsResponse;
 	}
 
 	@PutMapping(path="/{id}")
 	public FoodDetailsResponse updateFood(@PathVariable String id, @RequestBody FoodDetailsRequestModel foodDetails) throws Exception{
 
-		return null;
+		FoodEntity foodEntity = foodRepository.findById(Long.parseLong(id)).get();
+		String foodId = foodEntity.getFoodId();
+
+		FoodDto foodDto = new FoodDto();
+		foodDto.setFoodCategory(foodDetails.getFoodCategory());
+		foodDto.setFoodName(foodDetails.getFoodName());
+		foodDto.setFoodPrice(foodDetails.getFoodPrice());
+		foodDto.setFoodId(foodId);
+
+		FoodDto foodDto1 = foodService.updateFoodDetails(foodId,foodDto);
+
+		FoodDetailsResponse foodDetailsResponse = new FoodDetailsResponse();
+		foodDetailsResponse.setFoodId(foodId);
+		foodDetailsResponse.setFoodCategory(foodDto1.getFoodCategory());
+		foodDetailsResponse.setFoodPrice(foodDto1.getFoodPrice());
+		foodDetailsResponse.setFoodName(foodDto1.getFoodName());
+
+		return foodDetailsResponse;
 	}
 
 	@DeleteMapping(path = "/{id}")
 	public OperationStatusModel deleteFood(@PathVariable String id) throws Exception{
 
-		return null;
+		OperationStatusModel operationStatusModel = new OperationStatusModel();
+
+		try {
+			foodService.deleteFoodItem(id);
+			operationStatusModel.setOperationResult("Delete");
+			operationStatusModel.setOperationResult("Success");
+			return operationStatusModel;
+		}catch (Exception e){
+			operationStatusModel.setOperationResult("Delete");
+			operationStatusModel.setOperationResult("Failed");
+			return operationStatusModel;
+		}
 	}
-	
+
 	@GetMapping()
 	public List<FoodDetailsResponse> getFoods() {
 
-		return null;
+		List<FoodDto>foodDtoList = foodService.getFoods();
+
+		List<FoodDetailsResponse>foodDetailsResponseList = new ArrayList<>();
+
+		for(FoodDto foodDto:foodDtoList){
+			FoodDetailsResponse foodDetailsResponse = new FoodDetailsResponse();
+			foodDetailsResponse.setFoodName(foodDto.getFoodName());
+			foodDetailsResponse.setFoodPrice(foodDto.getFoodPrice());
+			foodDetailsResponse.setFoodCategory(foodDto.getFoodCategory());
+			foodDetailsResponse.setFoodId(foodDto.getFoodId());
+
+			foodDetailsResponseList.add(foodDetailsResponse);
+		}
+
+		return foodDetailsResponseList;
 	}
 }
